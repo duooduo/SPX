70650dd55c0b8fe2e1f64044f41d52b5b708e4ac
White <duoooduo@sina.com>
2016-07-29 23:15:11 +0800

    add ajax

--- js/shophome.js
+++ js/shophome.js
@@ -16,7 +16,16 @@
 		success: function (d) {
 			console.log(d);
 			if(d.status == 1001){
-				// todo
+				var data = d.data;
+				$('.shop_header img').attr('src',data.icon);    //头像
+				$('.shop_header_name').find('span').eq(0).html(data.realName);  //名字
+				$('.shop_header_name').find('span').eq(1).html(data.position);  //职位
+				if(data.sex == '男'){
+
+				}else if(data.sex == '女'){
+
+				}
+				$('.ovh .age').html(data.age);  //年龄
 
 			}
 		},
